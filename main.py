48a417dbae8765117065950463e990174a8ecb17
Deepak goyal <113086968+deepakgoyal10@users.noreply.github.com>
2022-11-08 11:58:24 +0530

    Update and rename library project.py to main.py

--- main.py
+++ main.py
@@ -6,43 +6,46 @@ class Library:
 
     def displaybooks(self):
         print(f"\nwelcome to {self.name} ")
-        print(f"These are the books we have: {self.booklist}")
+        # print(f"These are the books we have: {self.booklist}"
+        for book in booklist:
+            print(book)
 
     def addbook(self):
         book = input("Enter the name of book you want to add: ").capitalize()
         self.booklist.append(book)
+        print("Book added successfully.")
 
     def lendbook(self):
         try:
             book = input("Enter the book name: ")
-            # print(self.lendrecord)
             if book not in self.booklist:
-                # print(self.lendrecord.get(book))
                 print(f"This book isn't in library. Taken by {self.lendrecord.get(book)}")
             if book in self.booklist:
                 name = input("Enter your name: ")
                 self.booklist.remove(book)
                 self.lendrecord[book] = name
+                print("library successfully updated. You can take the book now.")
         except Exception as e:
                 print(e)
                 print("Enter the valid input!")
 
     def returnbook(self):
-        book = input("Enter the name of book: ")
+        book = input("Enter the name of book you want to return: ")
         if book in self.lendrecord:
             self.booklist.append(book)
             del self.lendrecord[book]
+            print("Library updated successfully.")
         else:
             print("Enter the valid input!")
 
 
-booklist = ["book1", "book2", "book3", "book4"]
 
-lib = Library(booklist, "Deepak library")
 
-# print(lib.addbook())
-# print(lib.booklist)
 if __name__ == '__main__':
+    booklist = ["book1", "book2", "book3", "book4"]
+
+    lib = Library(booklist, "Deepak library")
+
     while True:
         lib.displaybooks()
         user = input("press 1 to add book\npress 2 to lend book\npress 3 to return book\npress 0 to quit\n ")
@@ -54,3 +57,4 @@ if __name__ == '__main__':
             lib.returnbook()
         if user == "0":
             break
+
